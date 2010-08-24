10d66d61ccdaf0fe3f11d1ffc67ce72d0aa43d47
Serg Puhoff <github@puhoff.com>
2010-08-24 10:32:30 +0400

    Add error notification

--- lib/sfJabber.class.php
+++ lib/sfJabber.class.php
@@ -1,7 +1,7 @@
 <?php
 class sfJabber
 {
-  public static function SendMessage($msg, $to)
+  public static function SendMessage($msg, $to = NULL)
   {
     if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
     {
@@ -10,6 +10,9 @@ class sfJabber
       $username      = sfConfig::get('app_sfJabberPlugin_username');
       $host          = sfConfig::get('app_sfJabberPlugin_host');
       $password      = sfConfig::get('app_sfJabberPlugin_password');
+
+      if ($to == NULL)
+        $to = sfConfig::get('app_sfJabberPlugin_admin');
 
       $conn = new XMPPHP_XMPP($jabber_server, $port, $username, $password, 'xmpphp', $host, $printlog=false, $loglevel=XMPPHP_Log::LEVEL_VERBOSE);
       try
@@ -25,5 +28,14 @@ class sfJabber
       {
       }
     }
+  }
+
+  public static function SendExceptionNotify(sfEvent $event)
+  {
+    $routing = sfContext::getInstance()->getRouting();
+    $uri = $routing->getCurrentInternalUri();
+
+    $text = 'Ошибка: '.$event->getSubject()->getMessage().' по адресу '.$uri;
+    sfJabber::SendMessage($text);
   }
 }
